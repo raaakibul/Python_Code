65660955792c71036fc2ce1a50d0550f8a4d1d16
rakibul-islam <raakibul.uap@gmail.com>
2022-01-17 21:16:30 +0600

    List added

---
+++ list.py
@@ -0,0 +1,13 @@
+subjects = ["C", "C++", "Python", "Java", "mathlab"]
+print(subjects)
+print(subjects[0])
+print(subjects[2:])
+print(subjects[-1])
+print(subjects[-1:])
+
+print("C" in subjects)
+print("c" in subjects)
+
+print("c" not in subjects)
+
+print(subjects + ["swift", "Toc"])
