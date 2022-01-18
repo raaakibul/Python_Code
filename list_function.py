f7c29118be9a7cc05c57c5bace7e7fc49d52d427
rakibul-islam <raakibul.uap@gmail.com>
2022-01-19 01:28:17 +0600

    list function

---
+++ list_function.py
@@ -0,0 +1,14 @@
+subjects = ["C", "C++", "Python", "Java", "mathlab"]
+
+print(len(subjects))
+#append
+subjects.append("Computer")
+print(subjects)      
+# insert 
+subjects.insert(2,"Networking")
+print(subjects)
+
+#remove 
+subjects.remove("C")
+print(subjects)
+print(subjects.sort())
